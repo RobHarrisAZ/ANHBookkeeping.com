07ac2fdb1d8b0cc5f9b9ac5d201ebd394f6b15fb
RobHarrisAZ <monte96_@hotmail.com>
2013-03-04 22:28:20 -0700

    Initial Setup

---
+++ ANHBookkeeping.com/scripts/site.js
@@ -0,0 +1,11 @@
+﻿/// <reference path="jquery-1.9.1.min.js" />
+/// <reference path="angular.min.js" />
+/// <reference path="kendo/2012.3.1114/kendo.web.min.js" />
+var ANH = ANH || {};
+ANH.controllers = angular.module('NavController', ['$routeParams']);
+ANH.app = angular.module('ANH', ['NavController']).
+    config(
+    );
+$(document).ready(function() {
+    $('#mainMenu').kendoMenu({ orientation: 'horizontal' });
+});
